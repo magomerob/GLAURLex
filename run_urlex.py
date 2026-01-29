ec1e413ab2edc279b39744c678415571b451462e
magomerob <mark.mg02@gmail.com>
2026-01-29 12:02:21 +0100

    Fix ruta app

--- run_urlex.py
+++ run_urlex.py
@@ -2,9 +2,12 @@ import sys
 
 from streamlit.web import cli as stcli
 
+from urlex.ui import app as urlex_app
+
 
 def main() -> int:
-    sys.argv = ["streamlit", "run", "src/urlex/ui/app.py"]
+    app_path = urlex_app.__file__
+    sys.argv = ["streamlit", "run", app_path]
     return stcli.main()
 
 
